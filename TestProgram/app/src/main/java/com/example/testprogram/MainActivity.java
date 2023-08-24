4eab7230a9c453db99d6d43afdace2c40bb48ed8
10Misha10 <95186024+10Misha10@users.noreply.github.com>
2023-08-24 21:30:35 +0300

    apload

--- TestProgram/app/src/main/java/com/example/testprogram/MainActivity.java
+++ TestProgram/app/src/main/java/com/example/testprogram/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.testprogram;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView text;
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+        editText = findViewById(R.id.editTextText);
+        button = findViewById(R.id.button);
+
     }
+
+    public void startNewActivity(View v){
+            Intent intent = new Intent(this, Activity2.class);
+            startActivity(intent);
+    }
+
 }
