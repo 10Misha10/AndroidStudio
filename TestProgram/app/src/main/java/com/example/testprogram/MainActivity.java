0fbf41688f0a025ef7e117cd3632a430385ca4ab
10Misha10 <95186024+10Misha10@users.noreply.github.com>
2023-08-25 18:47:18 +0300

    upload_2

--- TestProgram/app/src/main/java/com/example/testprogram/MainActivity.java
+++ TestProgram/app/src/main/java/com/example/testprogram/MainActivity.java
@@ -7,15 +7,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView text;
-    private EditText editText;
-    private Button button;
+    private String[] arr = new String[]{"bulbasaur", "ivysaur", "venusaur", "charmander",
+            "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie",
+            "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto",
+            "pidgeot", "rattata", "raticate"};
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         text = findViewById(R.id.textView);
-        editText = findViewById(R.id.editTextText);
-        button = findViewById(R.id.button);
 
+        listView = findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.name_item, R.id.pokemon_name, arr);
+        listView.setAdapter(arrayAdapter);
     }
 
     public void startNewActivity(View v){
